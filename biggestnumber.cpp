334db7a9b0c4d94758684af95ada4f9b145b87b6
Mkuway <mkuway@mail.csuchico.edu>
2018-09-10 17:46:10 -0700

    This versiondetermines which of 2 numbers is greater

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -5,6 +5,7 @@ int main()
 {
   int number1;
   int number2;        
+  int biggest;
   //declare variables here
 
   std::cout<<"Please enter a whole number:\n";
@@ -17,9 +18,16 @@ int main()
   //get second input using std::cin>> again with a second variable
 
   //determine if first or second is bigger and store that in a third variable
-
-
+ if(number1 < number2)
+ {
+         std::cout<<"Of those numbers, the biggest is: ";
+         std::cout<<number2;
+ }
+ else
+ {
   std::cout<<"Of those two numbers, the biggest is: ";
+  std::cout<<number1;
+ }
   //print out the variable representing the biggest number
 
   std::cout<<std::endl<<"Thank you for playing.\n";
